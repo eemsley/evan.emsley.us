272f37213f81cc8a6d8690af08cf6ebcc672d9bf
eemsley <eemsley@purdue.edu>
2024-05-07 19:30:21 -0400

    feat: updated homepage

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="flex h-screen bg-green-100 w-full justify-center items-center">
-      <p className=" text-lg">Coming Soon!</p>
-      <Header />
+      <p className=" text-lg">Coming Soon! Does actions work? maybe!</p>
     </div>
   );
 }
