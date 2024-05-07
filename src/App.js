66c5f191bbb13f238c53ea1232fa5e4931be65a6
eemsley <eemsley@purdue.edu>
2024-05-07 19:26:30 -0400

    testing main branch (just linter)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="flex h-screen bg-green-100 w-full justify-center items-center">
       <p className=" text-lg">Coming Soon!</p>
+      <Header />
     </div>
   );
 }
